da3f32ffe398962f253360832316f0bf76944345
Adam Duff <j.adam.duff@gmail.com>
2018-08-31 08:12:17 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -6,4 +6,12 @@ class Show < ActiveRecord::Base
   def self.most_popular_show
     Show.find_by(rating: Show.maximum(:rating))
   end
+
+  def self.lowest_rating
+    Show.minimum(:rating)
+  end
+
+  def self.least_popular_show
+    Show.find_by(rating: Show.minimum(:rating))
+  end
 end
